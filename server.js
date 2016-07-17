1566731d0afcf34151fff328fbb467203363350e
dmfenton <dfenton@esri.com>
2016-07-17 16:21:28 -0400

    Add: server http and https

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-const tinylr = require('tiny-lr')
-
-const port = process.env.SERVER_PORT || 35729
-
-const options = {}
-
-if (process.env.LR_SSL_CRT && process.env.LR_SSL_KEY) {
-  const fs = require('fs')
-
-  options.key = fs.readFileSync(process.env.LR_SSL_KEY)
-  options.cert = fs.readFileSync(process.env.LR_SSL_CRT)
-	
-	console.log('SSL enabled')
-}
-
-const server = tinylr(options)
-
 process.on('SIGTERM', () => process.exit(0))
 
-server.on('GET /ember-cli-live-reload.js', (req, res) => {
-  res.setHeader('content-type', 'application/javascript')
-  res.write(`
-    (function() {
-       var src = 'http://localhost:${port}/livereload.js?snipver=1';
-       var script    = document.createElement('script');
-       script.type   = 'text/javascript';
-       script.src    = src;
-       document.getElementsByTagName('head')[0].appendChild(script);
-       }());`
-     )
-  res.end()
-})
+const tinylr = require('tiny-lr')
 
-server.listen(port, () => console.log(`Server listening on ${port}`))
+function createEmberScript (port) {
+  return `
+  (function() {
+     var src = 'http://localhost:${port}/livereload.js?snipver=1';
+     var script    = document.createElement('script');
+     script.type   = 'text/javascript';
+     script.src    = src;
+     document.getElementsByTagName('head')[0].appendChild(script);
+    }());`
+  }
+
+function createServer (options = {}, port = 35729) {
+  const server = tinylr(options)
+
+  server.on('GET /ember-cli-live-reload.js', (req, res) => {
+    res.setHeader('content-type', 'application/javascript')
+    const script = createEmberScript(port)
+    res.write(script)
+    res.end()
+  })
+
+  server.listen(port, () => console.log(`Server listening on ${port}`))
+
+  return server
+}
+
+// Create https server if ssl is enabled
+if (process.env.LR_SSL_CRT && process.env.LR_SSL_KEY) {
+  const fs = require('fs')
+  const port = process.env.SSL_SERVER_PORT || 35730
+  const options = {
+    key: fs.readFileSync(process.env.LR_SSL_KEY),
+    cert: fs.readFileSync(process.env.LR_SSL_CRT)
+  }
+
+	console.log('SSL enabled')
+  createServer(options, port)
+}
+
+// Create http server
+createServer(null, process.env.SERVER_PORT)
